1d150dab50970fcb749c59dfea4d4fbeee1c57d2
Armando Serna <armandoserna97@gmail.com>
2021-05-06 13:29:51 -0500

    Query movies data and show it in a table

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 using Vidly.Models;
 using Vidly.ViewModel;
 
@@ -10,6 +11,19 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _context.Dispose();
+        }
+
         private List<Movie> movies = new List<Movie>
         {
             new Movie { Id = 1, Name = "Shrek" },
@@ -18,6 +32,8 @@ namespace Vidly.Controllers
 
         public ActionResult Index()
         {
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
+
             var viewModel = new IndexMovieViewModel
             {
                 Movies = movies
