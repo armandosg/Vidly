81e73371521c03bb4797868ff80e90f89ccf2d7c
Armando Serna <armandoserna97@gmail.com>
2021-05-07 11:58:32 -0500

    Ignore customer Id on automapper profile

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -9,7 +9,8 @@ namespace Vidly.App_Start
         public MappingProfile()
         {
             Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<CustomerDto, Customer>()
+                  .ForMember(dest => dest.Id, act => act.Ignore());
         }
     }
 }
