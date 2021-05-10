31480e5a98c3f3e055eb605672df0902c0567847
Armando Serna <armandoserna97@gmail.com>
2021-05-10 13:49:50 -0500

    Add rentals

---
+++ Vidly/Controllers/Api/RentalsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.Api
+{
+    public class RentalsController : ApiController
+    {
+        // GET /rentals
+        public IHttpActionResult GetRentals()
+        {
+            return Ok();
+        }
+
+        // GET /rentals/1
+        public IHttpActionResult GetRental(int id)
+        {
+            return Ok();
+        }
+
+        // POST /rentals
+        [HttpPost]
+        public IHttpActionResult CreateRental(RentalDto rentalDto)
+        {
+            return Ok();
+        }
+
+        // PUT /rentals/1
+        [HttpPut]
+        public IHttpActionResult UpdateRental(int id)
+        {
+            return Ok();
+        }
+
+        // DELETE /rentals/1
+        [HttpDelete]
+        public IHttpActionResult DeleteRental(int id)
+        {
+            return Ok();
+        }
+    }
+}
